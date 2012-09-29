02625ee6909cd62f877b1a6f302c288bcce8203b
Trollhammaren <remcohaszing@gmail.com>
2012-09-29 20:18:01 +0200

    git conflicts fixed

---
+++ src/com/trollhammaren/odhinn/OnFileSelected.java
@@ -0,0 +1,8 @@
+package com.trollhammaren.odhinn;
+
+public interface OnFileSelected {
+    
+    public void directorySelected(int position);
+    
+    public void textFileSelected(int position);
+}
